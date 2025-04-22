bcbc934d63ab75ec59556020edc3282e1664ae8a
krishna4002 <krishnagopaljay@gmail.com>
2025-04-22 08:09:05 +0530

    Update App.py

--- App.py
+++ App.py
@@ -13,7 +13,7 @@ def process_image(image):
     test_img = cv2.resize(test_img, (224, 224))
     gray = cv2.cvtColor(test_img, cv2.COLOR_BGR2GRAY)
 
-    _, thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY_INV)
+    _, thresh = cv2.threshold(gray, 135, 255, cv2.THRESH_BINARY_INV)
     contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     count = 0
